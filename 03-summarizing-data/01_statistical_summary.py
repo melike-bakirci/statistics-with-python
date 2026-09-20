35c78b161629c1249afd51c7e128275f62e1426d
melike-bakirci <melike.bakirci@outlook.com>
2026-09-20 21:54:41 +0300

    Add statistical summary python script

---
+++ 03-summarizing-data/01_statistical_summary.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Veri seti
+data = [18, 19, 22, 23, 24, 25, 26, 27, 28, 28, 29, 30, 31, 32, 35, 38, 41, 45, 48, 52]
+
+# Pandas Series oluşturma
+s = pd.Series(data)
+
+# İstatistiksel özet
+summary = s.describe()
+
+print("Veri Seti İstatistiksel Özeti:\n")
+print(summary)
